8170d8c26e7bd492d52ccf1cc3047b127a1d4eb4
D3NCyan <edwarddoltons@gmail.com>
2022-01-01 18:09:36 +0800

    create post method, and add global data to CRUD

--- src/routes/api/todos.js
+++ src/routes/api/todos.js
@@ -1,7 +1,21 @@
+const data = []
+
 export const get = () => {
     return {
         body: {
-            name: 'dollong'
+            status: "success",
+            data
+        }
+    }
+}
+
+export const post = (req) => {
+    data.push(JSON.parse(req.body))
+    const status = req.body ? "Success" : 'Error'
+    return {
+        body: {
+            status,
+            data
         }
     }
 }
