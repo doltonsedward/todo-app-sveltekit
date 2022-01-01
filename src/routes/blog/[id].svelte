3c5d470f886acc4f30332f8b860c44bae77da24f
D3NCyan <edwarddoltons@gmail.com>
2022-01-01 12:23:33 +0800

    create blog route and component

---
+++ src/routes/blog/[id].svelte
@@ -0,0 +1,18 @@
+<script context="module">
+    export const load = ({ params }) => {
+        const id = params.id
+        
+        return {
+            props: {
+                id
+            }
+        }
+    }
+</script>
+
+<script>
+    export let id
+</script>
+
+{id}
+<h1 class="text-4xl">Welcome to blog</h1>
