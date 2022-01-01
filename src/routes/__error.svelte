0350990259b5e3d6f974a750cf673c1c67ea26ed
D3NCyan <edwarddoltons@gmail.com>
2022-01-01 14:53:09 +0800

    create Error component

---
+++ src/routes/__error.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+    export const load = ({ error, status }) => ({
+        props: {
+            error: error,
+            status: status
+        }
+    })
+</script>
+
+<script>
+    export let error
+    export let status
+</script>
+
+<div class="text-center">
+    <h1 class="text-4xl">{error.name} {status}</h1>
+    <p>{error.message}</p>
+</div>
