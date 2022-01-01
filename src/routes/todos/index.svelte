3f40ba7c974730972e83dd93d72d73f5aae939f8
D3NCyan <edwarddoltons@gmail.com>
2022-01-01 20:02:18 +0800

    create handleUpdate for updating

--- src/routes/todos/index.svelte
+++ src/routes/todos/index.svelte
@@ -1,56 +1,82 @@
-
-
 <script>
-    import Head from '$lib/Head.svelte' 
-    import { onMount } from 'svelte';
-    
-    export let todoName = ''
-    let dataTodo = []
+	import Head from '$lib/Head.svelte';
+	import { onMount } from 'svelte';
 
-    const getDataTodos = async () => {
-        try {
-            const response = await fetch('/api/todos')
-            const todos = await response.json()
-            dataTodo = todos.data
-        } catch (error) {
-            console.log(error)
-        }
-    }
+	export let todoName = '';
+	let dataTodo = [];
 
-    onMount(() => {
-        getDataTodos()
-    })
+	const getDataTodos = async () => {
+		try {
+			const response = await fetch('/api/todos');
+			const todos = await response.json();
+			dataTodo = todos.data;
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    const handleSubmit = async (name) => {
-        try {
-            const config = {
-                method: "POST",
-                body: JSON.stringify({
-                    name
-                })
-            }
-            
-            await fetch('/api/todos', config)
-            getDataTodos()
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    
-    const handleCreateTodo = () => {
-        handleSubmit(todoName)
-        todoName = ''
-    }
+	onMount(() => {
+		getDataTodos();
+	});
+
+	const handleSubmit = async (name, id) => {
+		try {
+			const config = {
+				method: !id ? 'POST' : 'PUT',
+				body: JSON.stringify({
+					name,
+					status: 'waiting'
+				})
+			};
+
+			await fetch(`/api${!id ? '/todos' : '/todos' + id}`, config);
+			getDataTodos();
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleUpdate = async (id) => {
+		try {
+			const config = {
+				method: 'PUT',
+				body: JSON.stringify({
+					name: 'oke',
+					status: 'done'
+				})
+			};
+
+			await fetch('/api/todos/' + id, config);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleCreateTodo = () => {
+		handleSubmit(todoName);
+		todoName = '';
+	};
+
+	$: console.log(dataTodo);
 </script>
 
 <Head title="Todos" />
 <h1 class="text-center text-4xl">Todos</h1>
 <form on:submit|preventDefault={handleCreateTodo}>
-    <input class="mt-5 py-1.5 px-1.5 w-full rounded ring-offset-2 ring-2" type="text" bind:value={todoName} placeholder="Write your todo" />
+	<input
+		class="mt-5 py-1.5 px-1.5 w-full rounded ring-offset-2 ring-2"
+		type="text"
+		bind:value={todoName}
+		placeholder="Write your todo"
+	/>
 </form>
-
-<div>
-    {#each dataTodo as todo}
-        <p>{todo.name}</p>
-    {/each}
+<div class="mt-5">
+	{#each dataTodo as todo, id}
+		<div class="p-4 my-2 card shadow-lg">
+			<label on:click={() => handleUpdate(id)} class="flex">
+				<input type="checkbox" class="checkbox mr-3" />
+				<span>{todo.name}</span>
+			</label>
+		</div>
+	{/each}
 </div>
