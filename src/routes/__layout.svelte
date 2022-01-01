c8545d8e8d708daa6c6d1c1b96e36b4715659012
D3NCyan <edwarddoltons@gmail.com>
2022-01-01 12:22:44 +0800

    create layout svelte

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Header from '$lib/Header.svelte'
+</script>
+
+<div class="layout px-10 sm:px-20">
+    <Header />
+    <div class="main-app py-4">
+        <slot></slot>
+    </div>
+</div>
