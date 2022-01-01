b12815504a8dbbd8a6238b0b9bf552980b4f810e
D3NCyan <edwarddoltons@gmail.com>
2022-01-01 15:35:21 +0800

    add head, and styling button in error component

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -8,14 +8,16 @@
 </script>
 
 <script>
+    import Head from '$lib/Head.svelte'
     export let error
     export let status
 </script>
 
+<Head title={error.name} />
 <div class="error__pages text-center">
     <h1 class="text-5xl">{error.name} {status}</h1>
     <p class="text-lg">{error.message}</p>
-    <button on:click={() => history.back()}>Go back</button>
+    <button class="bg-indigo-400 text-white rounded py-2 mt-5 drop-shadow-xl hover:bg-indigo-500 hover:ring-offset-2 hover:ring-2" on:click={() => history.back()}>Go back</button>
 </div>
 
 <style>
@@ -25,7 +27,8 @@
         height: calc(100vh - var(--header-height));
     }
 
-    h1 {
-        margin-bottom: 10px;
+    h1 { margin-bottom: 10px }
+    button:hover {
+        transform: scale(1.12);
     }
 </style>
