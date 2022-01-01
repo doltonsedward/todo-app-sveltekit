341dbbc06cc480563c29a4a9168b9b5940a95aaf
D3NCyan <edwarddoltons@gmail.com>
2022-01-01 12:22:34 +0800

    create header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,10 @@
+<header>
+    <nav class="flex justify-between">
+        <h1>Todo app</h1>
+        <ul class="flex justify-between">
+            <li class="ml-4"><a href="/">Home</a></li>
+            <li class="ml-4"><a href="/todos">Todos</a></li>
+            <li class="ml-4"><a href="/blog">Blog</a></li>
+        </ul>
+    </nav>
+</header>
