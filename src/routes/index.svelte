b0759b7a01d9adaec4d14e409820a1e1aec729b0
D3NCyan <edwarddoltons@gmail.com>
2022-01-01 15:35:33 +0800

    add Head in index app

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,11 @@
+<script>
+  import Head from '$lib/Head.svelte'
+</script>
+
+<Head />
 <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-    <div>
-      <div class="text-xl font-medium text-black">ChitChat</div>
-      <p class="text-gray-500">You have a new message!</p>
-    </div>
+  <div>
+    <div class="text-xl font-medium text-black">ChitChat</div>
+    <p class="text-gray-500">You have a new message!</p>
   </div>
+</div>
