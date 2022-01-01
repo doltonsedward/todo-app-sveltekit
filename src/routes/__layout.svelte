4c391597176cffb24df703b24df70eae4a25824f
D3NCyan <edwarddoltons@gmail.com>
2022-01-01 18:09:02 +0800

    cleanup code in layout, remove useless code

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,9 @@
 <script>
-    import Header from '$lib/Header.svelte'
+	import '../app.css';
+	import Header from '$lib/Header.svelte';
 </script>
 
 <Header />
 <div class="main-app px-10 sm:px-20">
-    <slot></slot>
+	<slot />
 </div>
