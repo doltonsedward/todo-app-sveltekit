ec9aea108d451de261dbd00207aec3dfdfbf1ec2
D3NCyan <edwarddoltons@gmail.com>
2022-01-01 14:53:31 +0800

    remove layout and padding in layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,9 +2,7 @@
     import Header from '$lib/Header.svelte'
 </script>
 
-<div class="layout px-10 sm:px-20">
-    <Header />
-    <div class="main-app py-4">
-        <slot></slot>
-    </div>
+<Header />
+<div class="main-app py-4 px-10 sm:px-20">
+    <slot></slot>
 </div>
