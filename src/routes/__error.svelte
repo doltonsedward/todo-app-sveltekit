a0fe8c3893c7b229e189434cb7ab5f2c3254e93b
D3NCyan <edwarddoltons@gmail.com>
2022-01-01 15:14:58 +0800

    add styling in error component

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -12,7 +12,20 @@
     export let status
 </script>
 
-<div class="text-center">
-    <h1 class="text-4xl">{error.name} {status}</h1>
-    <p>{error.message}</p>
+<div class="error__pages text-center">
+    <h1 class="text-5xl">{error.name} {status}</h1>
+    <p class="text-lg">{error.message}</p>
+    <button on:click={() => history.back()}>Go back</button>
 </div>
+
+<style>
+    .error__pages {
+        display: grid;
+        place-content: center;
+        height: calc(100vh - var(--header-height));
+    }
+
+    h1 {
+        margin-bottom: 10px;
+    }
+</style>
